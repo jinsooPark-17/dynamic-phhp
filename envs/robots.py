4f6a9fcc89c0d159ed1eb5be44d3728ed0d24d45
LOG_ZEPHYRUS\jinso <js.park@utexas.edu>
2023-09-11 14:41:31 -0500

    1. Combine all controllers to AllinOne class 2. launch roslaunch from python

--- envs/robots.py
+++ envs/robots.py
@@ -14,74 +14,117 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import Twist, PoseWithCovarianceStamped
 from geometry_msgs.msg import PolygonStamped, Point32
 
-class MoveBase(object):
-    def __init__(self, id: str=""):
+def quaternion_to_yaw(q):
+    # roll = atan2( 2.0*(q.w*q.x + q.y*q.z), 1.0 - 2.0*(q.x*q.x + q.y*q.y) )
+    # pich = 2.0 * atan2(sqrt(1.0 + 2.0*(q.w*q.y - q.x*q.z)), sqrt(1.0 - 2.0*(q.w*q.y - q.x*q.z))) - PI/2.0
+    # pich = arcsin( 2.0*(q.w*q.y - q.x*q.z) )
+    yaw = atan2( 2.0*(q.w*q.z + q.x*q.y), 1.0 - 2.0*(q.y*q.y + q.z*q.z) )
+    return yaw
+
+class AllinOne(object):
+    def __init__(self, id: str = "", policy = None):
+        # define variables
         self.id = id
+        self.policy = policy
+
         self.goal = MoveBaseGoal()
+        self.pose = PoseWithCovarianceStamped().pose.pose   # Pose() msg
+        self.trajectory = np.zeros(shape=(0,3), dtype=np.float32)
+        self.traj_idx: int = 0
 
-        # define ROS move_base
-        self.move_base = SimpleActionClient( os.path.join(self.id, "move_base"), MoveBaseAction )
-        self.connected = self.move_base.wait_for_server(timeout=rospy.Duration(30.0))
-        # if not connected:
-            # raise TimeoutError("MoveBase not respond")
-        # define clear costmap service
-        self.__clear_costmap_srv = rospy.ServiceProxy(os.path.join(self.id, "move_base", "clear_costmaps"), Empty)
+        # Define data storage
+        self.raw_scan = torch.zeros(size=(640,), dtype=torch.float32)
+        self.hal_scan = torch.zeros(size=(640,), dtype=torch.float32)
+        self.cmd_vel  = torch.zeros(size=(  4,), dtype=torch.float32)
 
-    def goto(self, x: float, y: float, yaw: float, timeout: int=60):
-        self.goal.target_pose.header.frame_id = os.path.join(self.id, "level_mux_map")
-        self.goal.target_pose.header.stamp = rospy.Time.now() + rospy.Duration(timeout)
-        self.goal.target_pose.pose.position.x = x
-        self.goal.target_pose.pose.position.y = y
-        self.goal.target_pose.pose.orientation.z = sin(yaw/2.)
-        self.goal.target_pose.pose.orientation.w = cos(yaw/2.)
+        # Connect to ROS MoveBase
+        self.__move_base = SimpleActionClient(
+            os.path.join(self.id, "move_base"),
+            MoveBaseAction
+        )
+        self.connected = self.__move_base.wait_for_server(timeout=rospy.Duration(60.0))
 
-        self.move_base.send_goal(
-            goal        = self.goal,
-            active_cb   = self.active_cb,
-            feedback_cb = self.feedback_cb,
-            done_cb     = self.done_cb
+        # Define ROS services
+        self.__make_plan_srv = rospy.ServiceProxy(
+            os.path.join(self.id, "move_base", "make_plan"),    # "$ID/move_base/NavfnROS/make_plan"
+            GetPlan
+        )
+        self.__clear_costmap_srv = rospy.ServiceProxy(
+            os.path.join(self.id, "move_base", "clear_costmaps"),
+            Empty
+        )
+        self.__clear_hallucination_srv = rospy.ServiceProxy(
+            os.path.join(self.id, "clear_virtual_circles"),
+            Empty
         )
 
+        # Define ROS publisher
+        self.__pub_localize = rospy.Publisher(
+            os.path.join(self.id, "initialpose"),
+            PoseWithCovarianceStamped,
+            queue_size=10
+        )
+        self.__pub_hallucination = rospy.Publisher(
+            os.path.join(self.id, "add_circles"),
+            PolygonStamped,
+            queue_size=10
+        )
+
+        # Define ROS subscriber
+        self.__sub_raw_scan = rospy.Subscriber(
+            os.path.join(self.id, "scan_filtered"),
+            LaserScan, 
+            self.__raw_scan_cb
+        )
+        self.__sub_hal_scan = rospy.Subscriber(
+            os.path.join(self.id, "scan_hallucinated"),
+            LaserScan,
+            self.__hal_scan_cb
+        )
+        self.__sub_cmd_vel = rospy.Subscriber(
+            os.path.join(self.id, "cmd_vel"),
+            Twist,
+            self.__cmd_vel_cb
+        )
+
+    def __raw_scan_cb(self, msg):
+        self.raw_scan = torch.nan_to_num(
+            torch.tensor(msg.ranges, dtype=torch.float32) / (msg.range_max - msg.range_min),
+            nan = 0.0
+        )
+    def __hal_scan_cb(self, msg):
+        self.hal_scan = torch.nan_to_num(
+            torch.tensor(msg.ranges, dtype=torch.float32) / (msg.range_max - msg.range_min),
+            nan = 0.0
+        )
+    def __cmd_vel_cb(self, msg):
+        self.cmd_vel[0] = msg.linear.x
+        self.cmd_vel[1] = msg.angular.z
+        # store max value only
+        self.cmd_vel[2] = max(self.cmd_vel[2], msg.linear.x)
+        self.cmd_vel[3] = max(self.cmd_vel[3], msg.angular.z)
+
     def clear_costmap(self):
-        rospy.wait_for_service(os.path.join(self.id, "move_base", "clear_costmaps"))
+        rospy.wait_for_service( os.path.join(self.id, "move_base", "clear_costmaps") )
         try:
             self.__clear_costmap_srv()
         except rospy.ServiceException as e:
-            raise RuntimeError(e)
-
-    def stop(self):
-        self.move_base.cancel_all_goals()
-
-    def active_cb(self):
-        raise NotImplementedError("No active callback")
-    
-    def feedback_cb(self, feedback):
-        raise NotImplementedError("No feedback callback")
-    
-    def done_cb(self, state, result):
-        raise NotImplementedError("No done callback")
-    
-    def is_arrived(self):
-        """ Pending = 0, Active = 1, Done = 2 """
-        return self.move_base.simple_state == 2
-
-class Vanilla(MoveBase):
-    def __init__(self, id):
-        super().__init__(id)
-        self.ttd = None
-        self.success = False
-        self.curr_pose = None
-        self.trajectory, self.idx = np.zeros(shape=(0,3), dtype=np.float32), 0
-
-        # localize AMCL
-        self.pub_localize = rospy.Publisher(os.path.join(self.id, "initialpose"), PoseWithCovarianceStamped, queue_size=10)
-
-    def goto(self, x: float, y: float, yaw: float, timeout: float=60.0):
-        # store 20hz feedback
-        self.idx = 0
-        self.trajectory = np.zeros(shape=(int(20*(timeout+1)), 3), dtype=np.float32)
-
-        return super().goto(x, y, yaw, timeout)
+            raise RuntimeError(f"{self.id}: {e}")
+    def clear_hallucination(self):
+        rospy.wait_for_service( os.path.join(self.id, "clear_virtual_circles") )
+        try:
+            self.__clear_hallucination_srv()
+        except rospy.ServiceException as e:
+            raise RuntimeError(f"{self.id}: {e}")
+    def make_plan(self, req):
+        rospy.wait_for_service( os.path.join(self.id, "move_base", "NavfnROS", "make_plan") )
+        # rospy.wait_for_service( os.path.join(self.id, "move_base", "make_plan") )
+        try:
+            plan_msg = self.__make_plan_srv( req )
+            plan = np.array([[p.pose.position.x, p.pose.position.y] for p in plan_msg.plan.poses])
+            return plan
+        except rospy.ServiceException as e:
+            raise RuntimeError(f"{self.id}: {e}")
 
     def localize(self, x: float, y: float, yaw: float, var: float=0.01):
         msg = PoseWithCovarianceStamped()
@@ -100,191 +143,99 @@ class Vanilla(MoveBase):
             0.0, 0.0, 0.0, 0.0, var, 0.0,   # P
             0.0, 0.0, 0.0, 0.0, 0.0, var,   # Y
         ]
-
-        self.curr_pose = msg.pose.pose
+        self.pose = msg.pose.pose
 
         self.pub_localize.publish(msg)
 
-    def reset(self, x:float, y: float, yaw: float, var: float=0.01):
-        self.localize(x, y, yaw, var)
-        self.clear_costmap()
-
-    # Define behavior of robot
-    def active_cb(self):
-        self.success = False
-        self.ttd = rospy.Time.now()
-
-    def feedback_cb(self, feedback):
-        self.curr_pose = feedback.base_position.pose
-        x = self.curr_pose.position.x
-        y = self.curr_pose.position.y
-        q = self.curr_pose.orientation
-        yaw = atan2(2.0 * q.z * q.w, 1.0 - 2.0 * q.z * q.z)
-        self.trajectory[self.idx] = [x, y, yaw]
-        self.idx += 1
-
-        # cancel goal if move_base failed to arrive goal location by timeout
-        if feedback.base_position.header.stamp > self.goal.target_pose.header.stamp:
-            self.stop()
-    
-    def done_cb(self, state, result):
-        self.clear_costmap()
-        self.ttd = (rospy.Time.now() - self.ttd).to_sec()
-        self.success = (state == 3) # True only if state is SUCCEEDED
-        self.trajectory = self.trajectory[:self.idx]
-
-    def hallucinate(self, **kwargs):
-        """ Dummy function for simplicity """
-        return None, None, None
-
-class Baseline(Vanilla):
-    def __init__(self, id):
-        super().__init__(id)
-
-        # define perceptual hallucination
-        self.ROBOT_RADIUS = 0.4
-
-        self.make_plan_srv           = rospy.ServiceProxy(os.path.join(self.id, "move_base", "NavfnROS", "make_plan"), GetPlan)
-        self.clear_hallucination_srv = rospy.ServiceProxy(os.path.join(self.id, "clear_virtual_circles"), Empty)
-        self.pub_hallucination       = rospy.Publisher(os.path.join(self.id, "add_circles"), PolygonStamped, queue_size=10)
-
-    def clear_hallucination(self):
-        rospy.wait_for_service(os.path.join(self.id, "clear_virtual_circles"))
-        try:
-            self.clear_hallucination_srv()
-        except rospy.ServiceException as e:
-            raise RuntimeError(e)
-
-    def reset(self, x: float, y: float, yaw: float, var: float = 0.01):
-        self.clear_hallucination()
-        return super().reset(x, y, yaw, var)
-
-    def active_cb(self):
-        RADIUS = 0.5
-        # Get plan from move_base
+    def hallucinate(self):
+        raise NotImplementedError()
+    def perceptual_hallucination(self, radius=1.0, dr=0.05, p_min=0.2, p_max=0.8):
+        # Get plan as numpy array
         plan_req = GetPlanRequest()
-        plan_req.start.header.frame_id = plan_req.goal.header.frame_id = os.path.join(self.id, "level_mux_map")
-        plan_req.start.pose = self.curr_pose
-        plan_req.goal.pose = self.goal.target_pose.pose
+        plan_req.start = plan_req.goal = self.goal.target_pose
+        plan_req.start.pose = self.pose
+        plan_req.tolerance = 0.1
+        plan = self.make_plan(plan_req)
 
-        rospy.wait_for_service(os.path.join(self.id, "move_base", "make_plan"))
-        try:
-            plan_msg = self.make_plan_srv( plan_req )
-        except Exception as e:
-            raise RuntimeError(e)
-        plan = np.array([[p.pose.position.x, p.pose.position.y] for p in plan_msg.plan.poses])
-
-        # calculate center of virtual circles that block left half of the hallway
-        dist = np.cumsum( np.linalg.norm(plan[1:] - plan[:-1], axis=1) )
-        idx_bgn, idx_end = np.searchsorted(dist, [1.5, dist[-1]-1.5]) # (1.0 + 0.5)^2 - 1.0^2 ~ 1.12 <1.2 + 0.3s buffer
-        idx = slice(idx_bgn, idx_end, 4)    # install hallucination every 4 plan points.
+        # calculate location of virtual circles that block left half of plan
+        dist = np.cumsum( np.linalg.norm(plan[1:] - plan[:-1], axis=1) ) # monotonic increasing array
+        d_min = max(dist[-1]*p_min, 1.5)
+        d_max = min(dist[-1]*p_max, dist[-1]-1.5)
+        idx_bgn, idx_end = np.searchsorted(dist, [d_min, d_max])
 
         dx, dy = (plan[2:] - plan[:-2]).T
-        theta = (np.arctan2(dy, dx) + np.pi/2.)[idx_bgn:idx_end:4]
-        plan = plan[idx_bgn:idx_end:4]
-
-        centers = plan + (RADIUS + 0.05) * np.array([np.cos(theta), np.sin(theta)]).T
+        theta = np.arctan2(dy, dx)[idx_bgn:idx_end:4] + np.pi/2.
+        centers = plan[idx_bgn+1:idx_end+1:4] + (radius + dr) * np.array([np.cos(theta), np.sin(theta)]).T
 
         msg = PolygonStamped()
         msg.header.stamp = rospy.Time.now() + rospy.Duration(9999.9)
-        msg.polygon.points = [Point32(x, y, RADIUS) for x, y in centers]
-        self.pub_hallucination.publish(msg)
+        msg.polygon.points = [Point32(x, y, radius) for x, y in centers]
+        self.__pub_hallucination.publish(msg)
 
-        return super().active_cb()
-    
-    def done_cb(self, state, result):
-        # clear hallucination
-        self.clear_hallucination()
-        return super().done_cb(state, result)
+    def move(self, x: float, y: float, yaw: float, mode: str="vanilla", timeout: float=60.0, **kwargs):
+        # Store trajectory from 20hz feedback loop
+        self.trajectory = np.zeros(shape=(int(20*timeout+1), 3), dtype=np.float32)
+        self.traj_idx = 0
 
-class D_PHHP(Baseline):
-    def __init__(self, id, policy, network_dir):
-        super().__init__(id)
-        self.policy = policy
-        self.network_dir = network_dir
+        self.goal.target_pose.header.frame_id = os.path.join(self.id, "level_mux_map")
+        self.goal.target_pose.header.stamp = rospy.Time.now() + rospy.Duration(timeout)
+        self.goal.target_pose.pose.position.x = x
+        self.goal.target_pose.pose.position.y = y
+        self.goal.target_pose.pose.orientation.z = sin(yaw/2.)
+        self.goal.target_pose.pose.orientation.w = cos(yaw/2.)
 
-        # storage
-        self.raw_scan = torch.zeros(size=(640,), dtype=torch.float32)
-        self.hal_scan = torch.zeros(size=(640,), dtype=torch.float32)
-
-        # define subscriber
-        self.sub_raw_scan = rospy.Subscriber(os.path.join(self.id, "scan_filtered"), LaserScan, self.raw_scan_cb)
-        self.sub_hal_scan = rospy.Subscriber(os.path.join(self.id, "scan_hallucinated"), LaserScan, self.hal_scan_cb)
-
-    def raw_scan_cb(self, msg):
-        self.raw_scan = torch.nan_to_num(
-            torch.tensor(msg.ranges, dtype=torch.float32) / (msg.range_max - msg.range_min), 
-            nan=0.0
-        )
-
-    def hal_scan_cb(self, msg):
-        self.raw_scan = torch.nan_to_num(
-            torch.tensor(msg.ranges, dtype=torch.float32) / (msg.range_max - msg.range_min),
-            nan=0.0
-        )
-
-    def reset(self, x: float, y: float, yaw: float, var: float = 0.01):
-        # Load new policy from file
-        self.policy.load_state_dict( torch.load(self.network_dir) )
-        return super().reset(x, y, yaw, var)
-
-    def active_cb(self):
-        self.ttd = rospy.Time.now()
-    
-    def get_state(self):
-        
-        state = torch.zeros(size=(640*2+2+1,), dtype=torch.float32)
-
-        # Store scan information
-        state[0:640] = self.raw_scan
-        state[640:1280] = self.hal_scan
-
-        # Store velocity message
-        try:
-            vel_msg = rospy.wait_for_message(os.path.join(self.id, "cmd_vel"), Twist, timeout=0.1)
-            state[1280] = vel_msg.linear.x
-            state[1281] = vel_msg.angular.z
-        except rospy.ROSException as e:
+        if mode == "vanilla":
             pass
+        elif mode == "baseline":
+            self.perceptual_hallucination(radius=0.5, p_min=0.2, p_max=0.8)
+        elif mode == "d-phhp":
+            self.policy.load_state_dict( torch.load(kwargs['network_dir']) )# load policy
 
-        # Store distance to goal
-        x, y, yaw = self.trajectory[self.idx-1]
+        self.__move_base.send_goal(
+            goal        = self.goal,
+            # active_cb   = None,
+            feedback_cb = self.feedback_cb,
+            # done_cb     = None
+        )
+    def feedback_cb(self, feedback):
+        self.pose = feedback.base_position.pose
+        self.trajectory[self.traj_idx] = [
+            feedback.base_position.pose.position.x,
+            feedback.base_position.pose.position.y,
+            quaternion_to_yaw(feedback.base_position.pose.orientation)
+        ]
+        self.traj_idx = self.traj_idx + 1
+
+        # Timeout
+        if feedback.base_position.header.stamp > self.goal.target_pose.header.stamp:
+            self.stop()
+    def stop(self):
+        self.__move_base.cancel_all_goals()
+
+    def get_state(self, ):
+        state = torch.zeros(size=(640*2+2+1,), dtype=torch.float32)
+        # Store scan information
+        state[0:640]    = self.raw_scan
+        state[640:1280] = self.hal_scan
+        # Store velocity message
+        state[1280] = self.cmd_vel[0]
+        state[1281] = self.cmd_vel[1]
+        # state[1281] = self.cmd_vel[3]
+        self.cmd_vel.zero_()
+
+        # Store angle to goal
+        x, y, yaw = self.trajectory[self.traj_idx-1]
         gyaw = atan2( self.goal.target_pose.pose.position.y - y, self.goal.target_pose.pose.position.x - x )
         state[1282] = (gyaw - yaw + np.pi) / (2.*np.pi)
 
         return state
-
-    def hallucinate(self, explore=False):
-        state = self.get_state()
-        if explore is True:
-            action = np.random.uniform(low=-1.0, high=1.0, size=3)
-        else:
-            with torch.no_grad():
-                action, _ = self.policy(state)
-        reward = -1.0
-
-        # convert (r, th, t) -> (global_x, global_y, t)
-        r  = action[0]*4.0 + 5.0    #   1 ~  9
-        th = action[1]*np.pi        # -pi ~ pi
-        t  = action[2]*5.0 + 5.0    #   0 ~ 10
-
-        x, y, yaw = self.trajectory[self.idx-1]
-        global_x = x + r * cos(yaw + th)
-        global_y = y + r * sin(yaw + th)
-
-        msg = PolygonStamped()
-        msg.header.stamp = rospy.Time.now() + rospy.Duration(t)
-        msg.polygon.points = [Point32(global_x, global_y, 1.0)]
-        self.pub_hallucination.publish(msg)
-
-        return state, action, reward
 
 if __name__ == "__main__":
     # Only use for Debug!
     rospy.init_node("debug")
     rospy.sleep(1.0)
 
-    marvin = Vanilla(id='marvin')
+    marvin = AllinOne(id='marvin')
 
     marvin.goto(0, 0, 0, timeout=30)
 
@@ -296,3 +247,4 @@ if __name__ == "__main__":
     traj = marvin.trajectory[:marvin.idx]
     plt.scatter(traj.T[0], traj.T[1])
     plt.savefig("tt.png", dpi=300)
+
--- mpi_test_ep.py
+++ mpi_test_ep.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python3
 import os
+import time
 import mpi4py
 mpi4py.rc.recv_mprobe = False
 from mpi4py import MPI
 from socket import gethostname
+from subprocess import Popen, DEVNULL
 from collections import namedtuple
-from envs.robots import Vanilla
+from envs.robots import AllinOne
 from envs.simulation import L_Hallway_Single_robot
 import numpy as np
 import matplotlib.pyplot as plt
 
 Pose = namedtuple("Pose", "x y yaw")
-
 if __name__ == "__main__":
     # Initialize MPI
     comm = MPI.COMM_WORLD
     size = comm.Get_size()
     rank = comm.Get_rank()
-    
+    print(f"{gethostname}: {rank} MPI job is ready", flush=True)
+
+    # Launch ROS environment
+    time.sleep( rank % 12 ) # distribute work load to avoid heavy load
+    roslaunch = Popen([], stdout=DEVNULL) # stderr=DEVNULL
+    time.sleep( 60.0 )  # wait for roslaunch to fully deployed
+    comm.Barrier()
+    if rank == 0:
+        print(f"All simulation is launched.", flush=True)
+
     # Choose robot
     env = L_Hallway_Single_robot(ep_timeout=30.0)
     idx = np.random.choice(2)
     name, init_pose = [["marvin", Pose(-10,0,0)], ["rob", Pose(0, -10, np.pi/2.)]][idx]
-    robot1 = Vanilla( name )
+    robot1 = AllinOne( name )
     env.register_robots(robot1=robot1)
 
     # Print total number of available simulations
@@ -33,17 +43,17 @@ if __name__ == "__main__":
         print(f"Running episode with {n_valid}/{size} simulators", flush=True)
     
     # Now, run episode
-    ttd = env.begin([init_pose], [Pose(0., 0., np.pi/4.)])
-    if type(ttd) is float:
-        print(f"[{gethostname()}-{rank:02d}] Episode TTD: {ttd:.2f} s", flush=True)
+    if valid is True:
+        ttd = env.begin([init_pose], [Pose(0., 0., np.pi/4.)])
+        if type(ttd) is float:
+            print(f"[{gethostname()}-{rank:02d}] Episode TTD: {ttd:.2f} s", flush=True)
+        else:
+            print(f"Simulator {rank+1} failed.")
     else:
-        print(f"Simulator {rank+1} failed.")
+        ttd = 0.0
 
     avg_ep_ttd = comm.reduce(ttd, op=MPI.SUM, root=0)
 
     if rank == 0:
         avg_ep_ttd = avg_ep_ttd / float(n_valid)
         print(f"\nAverage TTD of single episode: {avg_ep_ttd:.3f} seconds", flush=True)
-
-    comm.Barrier()
-    os.system("killall rosmaster")  # Kill ROS to purge sbatch job
