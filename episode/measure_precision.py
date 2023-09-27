2a9ebc0e3accc6eef15136aab2025e2d6fc4746b
LOG_ZEPHYRUS\jinso <js.park@utexas.edu>
2023-09-27 01:24:58 -0500

    redirect ROS_LOG_DIR to local /tmp directory and fix np.save issue.

--- episode/measure_precision.py
+++ episode/measure_precision.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-import os
 import numpy as np
-import rospy
 import argparse
 from collections import namedtuple
 from envs.simulation import I_Shaped_Hallway
@@ -11,16 +9,20 @@ if __name__=="__main__":
     # Define argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument("storage", type=str, help="Name of singularity container instance")
+    parser.add_argument("num_test", type=int, help="Number of episodes")
     args = parser.parse_args()
 
     # Perform episode
     env = I_Shaped_Hallway()
-    d1, d2 = np.random.uniform(low=6.0, high=10.0, size=2)
-    init_poses = [Pose(-10.0   , 0.0, 0.0), Pose(0.0, 0.0   , -np.pi/2.)]
-    goal_poses = [Pose(-10.0+d1, 0.0, 0.0), Pose(0.0, 0.0-d2, -np.pi/2.)]
-    result = env.test_precision(init_poses, goal_poses, timeout=40.0)
-    result = np.hstack(([[d1],[d2]], result))
+
+    result = np.empty((2*args.num_test,4))
+    for i in range(args.num_test):
+        d1, d2 = np.random.uniform(low=6.0, high=10.0, size=2)
+        init_poses = [Pose(-10.0   , 0.0, 0.0), Pose(0.0, 0.0   , -np.pi/2.)]
+        goal_poses = [Pose(-10.0+d1, 0.0, 0.0), Pose(0.0, 0.0-d2, -np.pi/2.)]
+        ttd = env.test_precision(init_poses, goal_poses, timeout=40.0)
+        result[2*i:2*i+2] = np.hstack(([[d1],[d2]], ttd))
 
     # save numpy array to file
-    with open(args.storage, 'ab') as f:
+    with open(args.storage, 'w') as f:
         np.save(f, result)
--- test_movebase_precision.py
+++ test_movebase_precision.py
@@ -41,23 +41,28 @@ if __name__=='__main__':
         print(f"Maximum number of restart was {max_restart}.", flush=True)
 
     # Run episode
-    storage = f"/tmp/{ID}.result"
-    for i_ep in range(n_test):
-        ep_proc = subprocess.Popen(["singularity", "run", f"instance://{ID}", "python3", "episode/measure_precision.py", storage])
-        ep_proc.wait()
+    ep_proc = subprocess.Popen(["singularity", "run", f"instance://{ID}", 
+                                "python3", "episode/measure_precision.py", f"/tmp/{ID}.result", f"{n_test}"])
+    ep_proc.wait()
     comm.Barrier()
+    os.system(f"singularity instance stop {ID} > /dev/null 2>&1")
 
-    # Collect result to single file
+    # Read data from file storage
     with open(storage, 'rb') as f:
         data = np.load(f)
+    os.remove(storage)
+
+    # Collect data through MPI
     recv_arr = None
-    if rank==0: recv_arr = np.empty((size, n_test*2, 4))
+    if rank==0: recv_arr = np.empty((size, *data.shape))
     comm.Gather(data, recv_arr, root=0)
+
+    # Save data as csv file
     if rank==0:
         if not os.path.exists(f"{os.getenv('WORK')}/data"):
             os.makedirs(f"{os.getenv('WORK')}/data")
-        df = pd.DataFrame(recv_arr.reshape(-1,4), columns=["travel_dist", "ttd", "distance_error", "angle_error"])
-        df.to_csv(f"{os.getenv['WORK']}/data/movebase_precision_result.{os.getenv('SLURM_JOB_ID')}.csv")
+        df = pd.DataFrame(recv_arr.reshape(-1,data.shape[-1]), columns=["travel_dist", "ttd", "distance_error", "angle_error"])
+        df.to_csv(f"{os.getenv('WORK')}/data/movebase_precision_result.{os.getenv('SLURM_JOB_ID')}.csv")
 
         print(f"MoveBase precision experiment result with {size*n_test*2} episodes")
         print(df.mean(axis=0))
