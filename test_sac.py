e542c9f9de88d59da3f47df7cba68cdaf078ca51
LOG_ZEPHYRUS\jinso <js.park@utexas.edu>
2024-01-31 23:58:24 +0900

    Use configuration file to control training process.

--- test_sac.py
+++ test_sac.py
@@ -1,5 +1,7 @@
 import os
 import time
+import uuid
+import yaml
 import torch
 import argparse
 import subprocess
@@ -17,70 +19,82 @@ def run_episode(output_path, model_path, explore=False, test=False):
 
 def load_data(sample_path):
     data = torch.load(sample_path)
-    return (
-        torch.from_numpy(data['state']).to(torch.float32), 
-        torch.from_numpy(data['action']).to(torch.float32), 
-        torch.from_numpy(data['next_state']).to(torch.float32), 
-        torch.from_numpy(data['reward']).to(torch.float32), 
-        torch.from_numpy(data['done']).to(torch.float32) 
-    )
-
-OBS_DIM = 3
-ACT_DIM = 1
-STORAGE       = "network"
-MODEL_STORAGE = os.path.join(STORAGE, "pi.pt")
-TRAIN_SAMPLE  = "/tmp/train_sample.pt"
-TEST_SAMPLE   = "/tmp/test_sample.pt"
+    return (torch.from_numpy(data['state']).to(torch.float32), 
+            torch.from_numpy(data['action']).to(torch.float32), 
+            torch.from_numpy(data['next_state']).to(torch.float32), 
+            torch.from_numpy(data['reward']).to(torch.float32), 
+            torch.from_numpy(data['done']).to(torch.float32))
 
 if __name__ == '__main__':
     # Define argument parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("--replay_size", type=int, default=int(3e4))
-    parser.add_argument("--batch_size", type=int, default=256)
-    parser.add_argument("--gamma", type=float, default=0.99, help="(default: 0.99)")
-    parser.add_argument("--polyak", type=float, default=0.005, help="(default: 0.995)")
-    parser.add_argument("--lr", type=float, default=1e-3)
-    parser.add_argument("--alpha", type=float, default=0.2, help="(default: 0.2)")
-    parser.add_argument("--explore_steps", type=int, default=10000)
-    parser.add_argument("--update_after", type=int, default=1000)
+    parser.add_argument("--config", type=str, help="configuration file *.yml", required=True)
     args = parser.parse_args()
 
-    tensorboard = SummaryWriter("tensorboard/test")
-    replay = ReplayBuffer(obs_dim=OBS_DIM, act_dim=ACT_DIM, size=args.replay_size)
-    sac = SAC(actor_critic=MLPActorCritic(n_obs=OBS_DIM, n_act=ACT_DIM, hidden=(256,256,256,)),
-              gamma=args.gamma, polyak=args.polyak, lr=args.lr, alpha=args.alpha)
-    sac.save(STORAGE)
+    # Load configuration from yaml file
+    with open(args.config) as f:
+        config = yaml.safe_load(f)
+
+    # Define data storage
+    TASK_UUID     = str(uuid.uuid4())
+    SLURM_JOBID   = os.getenv("SLURM_JOBID", default='test')
+    MODEL_STORAGE = os.path.join("results", SLURM_JOBID, "network")
+    MODEL         = os.path.join("results", SLURM_JOBID, "network", "pi.pt")
+    TEST_EPISODE  = os.path.join(os.sep, "tmp", f"{TASK_UUID}.pt")
+    TRAIN_EPISODE = os.path.join(os.sep, "tmp", f"{TASK_UUID}.pt")
+    LOG_DIR       = os.path.join("results", SLURM_JOBID, "tensorboard")
+
+    # Prepare training
+    tensorboard = SummaryWriter(LOG_DIR)
+    sac = SAC(actor_critic=MLPActorCritic(n_obs=config["policy"]["obs_dim"], n_act=config["policy"]["act_dim"], hidden=(256,256,256,)),
+              gamma=config["SAC"]["gamma"], polyak=config["SAC"]["polyak"], lr=config["SAC"]["lr"], alpha=config["SAC"]["alpha"])
+    replay = ReplayBuffer(
+        obs_dim=config["policy"]["obs_dim"], 
+        act_dim=config["policy"]["act_dim"], 
+        size=config["train"]["replay_size"]
+    )
+    sac.save(MODEL_STORAGE)
 
     # Main loop
     ## Run first pure exploration episode & test episode
-    train_episode = run_episode(output_path=TRAIN_SAMPLE, model_path=MODEL_STORAGE, explore=True)
+    train_episode = run_episode(output_path=TRAIN_EPISODE, model_path=MODEL, explore=True)
     train_episode.wait()
 
     total_steps = 0
-    for epoch in range(100):
+    for epoch in range(config["epoch"]):
         start_time = time.time()
-        while total_steps // 1000 == epoch:
-            train_episode = run_episode(output_path=TRAIN_SAMPLE, model_path=MODEL_STORAGE, explore=(True if total_steps < args.explore_steps else False))
+        while total_steps // config["steps_per_epoch"] == epoch:
+            # Begin episode
+            if total_steps < config["train"]["explore_steps"]:
+                train_episode = run_episode(output_path=TRAIN_EPISODE, model_path=MODEL, explore=True)
+            else:
+                train_episode = run_episode(output_path=TRAIN_EPISODE, model_path=MODEL, explore=False)
 
-            # Store episode information to replay buffer and tensorboard
-            s, a, ns, r, d = load_data(TRAIN_SAMPLE)
+            # Use previous episode to train policy
+            s, a, ns, r, d = load_data(TRAIN_EPISODE)
             ep_len = replay.store(s, a, ns, r, d)
             total_steps += ep_len
             tensorboard.add_scalars('reward', {'train': r.sum()}, total_steps)
 
-            if total_steps > args.update_after:
+            if total_steps > config["train"]["update_after"]:
                 for k in range(total_steps-ep_len, total_steps):
-                    q_info, pi_info = sac.update(batch=replay.sample_batch(args.batch_size))
+                    batch = replay.sample_batch(config["train"]["batch_size"])
+                    q_info, pi_info = sac.update(batch=batch)
                     tensorboard.add_scalar("Loss/Pi", pi_info.detach().item(), k)
                     tensorboard.add_scalar("Loss/Q",  q_info.detach().item(), k)
-                sac.save(STORAGE)
+                sac.save(MODEL_STORAGE)
             
-            # wait for the next episode
+            # Wait for the next episode
             train_episode.wait()
 
-        # When one episode ends, run test episode
-        test_episode = run_episode(output_path=TEST_SAMPLE, model_path=MODEL_STORAGE, test=True)
+        # For each Epoch, run test episode to show training progress
+        test_episode = run_episode(output_path=TEST_EPISODE, model_path=MODEL, test=True)
         test_episode.wait()
-        _, _, _, r, d = load_data(TEST_SAMPLE)
+        _, _, _, r, d = load_data(TEST_EPISODE)
         tensorboard.add_scalars("reward", {'test': r.sum()}, total_steps)
+
+        # Save policy for every $SAVE_FREQUENCY epochs
+        if (epoch+1) % config["save_frequency"] == 0:
+            sac.save(os.path.join(MODEL_STORAGE, str(epoch+1)))
+
         print(f"Epoch {epoch+1} took {time.time() - start_time:.2f} seconds.")
