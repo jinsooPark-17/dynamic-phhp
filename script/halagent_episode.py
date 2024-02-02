2192bd5678d5c7c33bbd941fa151e8d222b9de67
LOG_ZEPHYRUS\jinso <js.park@utexas.edu>
2024-02-03 00:27:32 +0900

    Test single task halagent train script v.0.0.1

--- script/halagent_episode.py
+++ script/halagent_episode.py
@@ -1,3 +1,6 @@
+import os
+import time
+import yaml
 import argparse
 import numpy as np
 import torch
@@ -27,86 +30,88 @@ def generate_random_episode():
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("output_file_path", type=str, default='test', required=True)
-    parser.add_argument("--network", type=torch.load, default=None)
-    parser.add_argument("--n_scan", type=int, default=1, required=True, 
-                        help="Defines the number of raw or hallucinated scans incorporated into the state space. (default: 1)")
-    parser.add_argument("--sensor_horizon", type=float, default=8.0, required=True,
-                        help="Specifies the maximum distance, in meters, for LiDAR sensor readings to be considered relevant; beyond this, data is ignored. (default: 8.0 m)")
-    parser.add_argument("--plan_interval", type=float, required=True,
-                        help="Configures the interval for plan quantization; the policy will utilize this quantized plan for decision-making. (default: 0.5 m)")
-    parser.add_argument("--policy_hz", type=float, required=True,
-                        help="Sets how often the HalAgent policy is applied each second, allowing for dynamic adjustment to system behavior. (default: 2.0 hz)")
-    parser.add_argument("--opponent", type=str, choices=["vanilla", "baseline", "phhp"], required=True,
-                        help="Select type of opponent robot that HalAgent must pass. [vanilla, baseline, phhp]")
-    parser.add_argument("--combine_scans", action='store_true',
-                        help="If activated, raw scan and hallucination scan will be combined to create features")
-    parser.add_argument("--include_radius", action='store_true',
-                        help="If activated, policy will determine (install, x, y, t, r) otherwise (install, x, y, t)")
+    parser.add_argument("--network_dir", type=str, required=True)
+    parser.add_argument("--config", type=str, help="configuration file *.yml", required=True)
+    parser.add_argument("--command", type=str)
     parser.add_argument("--test", action='store_true',
                         help="Activate when testing a trained policy.")
-    parser.add_argument("--explore", action='store_true',
-                        help="If, Activate the episode becomes pure exploration episode.")
     args = parser.parse_args()
 
+    # Load configurations
+    with open(args.config) as f:
+        config = yaml.safe_load(f)
+
     # Define constant
-    N_PLAN = int(args.sensor_horizon/args.plan_interval*2)
-    N_STATE = 2 * args.n_scan * 640 + N_PLAN + 2    # scan / plan / vw
-    N_ACTION = (5 if args.include_radius else 4)
-    MAX_SAMPLES = int(60.0 * args.policy_hz) + 1
+    N_PLAN = int(args.sensor_horizon/args.plan_interval * 2.)
+    N_STATE = 2*args.n_scan*640 + N_PLAN + 2        # scan / plan / vw
+    MAX_SAMPLES = int(config["episode"]["timeout"] * args.policy_hz) + 1
 
     env = HallwayEpisode(
-        num_scan_history=args.n_scan, 
-        sensor_horizon=args.sensor_horizon, 
-        plan_interval=args.plan_interval, 
-        policy_hz=args.policy_hz
+        num_scan_history=config["n_scan"], 
+        sensor_horizon=config["sensor_horizon"], 
+        plan_interval=config["plan_interval"], 
+        policy_hz=config["policy_hz"]
     )
     policy = ActorCritic(
-        n_scan=args.n_scan,
+        n_scan=config["n_scan"],
         n_plan=N_PLAN,
-        action_dim=N_ACTION,
-        combine=args.combine_scans
-    )
-    if args.network_dir is not None:
-        policy.load(args.network_dir)
-
-    episode_info = dict(
-        state      = np.zeros((MAX_SAMPLES, N_STATE), dtype=np.float32), 
-        action     = np.zeros((MAX_SAMPLES, N_ACTION), dtype=np.float32),
-        next_state = np.zeros((MAX_SAMPLES, N_STATE), dtype=np.float32),
-        reward     = np.zeros(MAX_SAMPLES, dtype=np.float32),
-        done       = np.zeros(MAX_SAMPLES, dtype=np.float32)
+        action_dim=config["act_dim"],
+        combine=config["combine_scans"]
     )
 
-    # Begin episode
-    init_poses, goal_poses = generate_random_episode()
-    state = env.reset(
-        robot_modes=['vanilla', args.opponent],
-        init_poses=init_poses, 
-        goal_poses=goal_poses
-    )
-    done, idx = False, 0
-    while not done:
-        if args.explore is True:
-            action = np.random.uniform(-1.0, 1.0, N_ACTION)
-        else:
-            action = policy.act(state, deterministic=args.test)
-        next_state, reward, done = env.step(action)
+    # Define data storage
+    state      = np.zeros((MAX_SAMPLES, N_STATE)), 
+    action     = np.zeros((MAX_SAMPLES, config["act_dim"])),
+    next_state = np.zeros((MAX_SAMPLES, N_STATE)),
+    reward     = np.zeros(MAX_SAMPLES),
+    done       = np.zeros(MAX_SAMPLES)
 
-        # Store episode data
-        episode_info['state'][idx]      = state
-        episode_info['action'][idx]     = action
-        episode_info['next_state'][idx] = next_state
-        episode_info['reward'][idx]     = reward
-        episode_info['done'][idx]       = done
+    try:
+        while True:
+            # wait for the training process
+            while os.path.exists(args.output_file_path):
+                time.sleep(0.1)
+            
+            # Load new model
+            policy.load_state_dict(torch.load(args.network_dir))
 
-        state = next_state
-        idx += 1
-    
-    # Store information as a file
-    np.savez(args.output_file_path, 
-             state=episode_info['state'][:idx],
-             action=episode_info['action'][:idx],
-             next_state=episode_info['next_state'][:idx],
-             reward=episode_info['reward'][:idx],
-             done=episode_info['done'][:idx])
-    env.close()
+            # Begin episode
+            init_poses, goal_poses = generate_random_episode()
+            with open(os.path.join(args.commands, "opponent.command"), 'r') as f:
+                opponent = f.read()
+
+            state = env.reset(
+                robot_modes=['vanilla', opponent],   # Get command from train proc
+                init_poses=init_poses, 
+                goal_poses=goal_poses
+            )
+            d, idx = False, 0
+            while not d:
+                if os.path.exists(os.path.join(args.commands, "explore.command")):
+                    a = np.random.uniform(-1.0, 1.0, config["act_dim"])
+                else:
+                    s = torch.from_numpy(a).to(torch.float32)
+                    a, _ = policy(s, deterministic=args.test)
+                    a = a.numpy()
+                ns, r, d = env.step(a)
+
+                # Store SAS'RD
+                state[idx]      = s
+                action[idx]     = a
+                next_state[idx] = ns
+                reward[idx]     = r
+                done[idx]       = d
+
+                # Update
+                s = ns
+                idx = idx + 1
+            
+            # Store episode information as a file
+            episode_info = dict(state=torch.from_numpy(state[:idx]).to(torch.float32),
+                                action=torch.from_numpy(action[:idx]).to(torch.float32),
+                                next_state=torch.from_numpy(next_state[:idx]).to(torch.float32),
+                                reward=torch.from_numpy(reward[:idx]).to(torch.float32),
+                                done=torch.from_numpy(done[:idx]).to(torch.float32))
+            torch.save(episode_info, args.output_file_path)
+    finally:
+        env.close()
---
+++ train_halagent.py
@@ -0,0 +1,150 @@
+import os
+import time
+import uuid
+import yaml
+import torch
+import argparse
+import subprocess
+from script.network import ActorCritic
+from rl.sac import ReplayBuffer, SAC
+from torch.utils.tensorboard import SummaryWriter
+
+def launch_simulation(uuid, args=''):
+    # Initiate ROS-Gazebo simulation
+    for n_restart in range(10): # Max restart 10 times
+        os.system(f"singularity instance start --net --network=none {os.getenv('CONTAINER')} {uuid} {args}")
+        time.sleep(5.0)
+        test_proc = subprocess.Popen(["singularity", "run", f"instance://{uuid}", "/wait_until_stable"])
+        try:
+            test_proc.wait( timeout=60.0 )
+        except subprocess.TimeoutExpired as e:
+            print(f"Restarting {uuid}...", flush=True)
+            os.system(f"singularity instance stop {uuid} > /dev/null 2>&1")
+        else:
+            return
+    raise RuntimeError("Simulation failed to launched after 10 trials. Stop training process.")
+
+def run_episode(output_path, model_path, explore=None, test=False):
+    commands = ["python3", "script/halagent_episode.py", output_path, "--network_dir", model_path]
+    if not explore is None:
+        commands += ["--explore", explore]
+    elif test:
+        commands += ["--test"]
+    return subprocess.Popen(commands)
+
+def load_data(sample_path):
+    data = torch.load(sample_path)
+    os.remove(sample_path)
+    return data['state'], data['action'], data['next_state'], data['reward'], data['done']
+
+def random_opponent(opponent_list):
+    opponent_list = opponent_list
+    def gen_command():
+        idx = torch.randint(0, len(opponent_list), (1,)).item()
+        with open("opponent.command", 'w') as f:
+            f.write(opponent_list[idx])
+    return gen_command
+
+if __name__ == '__main__':
+    # Define argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, help="configuration file *.yml", required=True)
+    args = parser.parse_args()
+
+    # Load configuration from yaml file
+    with open(args.config) as f:
+        config = yaml.safe_load(f)
+    N_PLAN = int(config["policy"]["sensor_horizon"]/config["policy"]["plan_interval"]*2.)
+    config['n_plan'] = N_PLAN
+    choose_opponent = random_opponent(config["episode"]["opponents"])
+
+    # Define data storage
+    TASK_UUID     = str(uuid.uuid4())
+    SLURM_JOBID   = os.getenv("SLURM_JOBID", default='test')
+    MODEL_STORAGE = os.path.join("results", SLURM_JOBID, "network")
+    MODEL         = os.path.join("results", SLURM_JOBID, "network", "pi.pt")
+    TEST_EPISODE  = os.path.join(os.sep, "tmp", f"{TASK_UUID}.pt")
+    TRAIN_EPISODE = os.path.join(os.sep, "tmp", f"{TASK_UUID}.pt")
+    LOG_DIR       = os.path.join("results", SLURM_JOBID, "tensorboard")
+    EXPLORE_CMD   = os.path.join("results", SLURM_JOBID, "explore.command")
+    
+    # Prepare training
+    tensorboard = SummaryWriter(LOG_DIR)
+    actor_critic = ActorCritic(n_scan=config["policy"]["n_scan"],
+                               n_plan=N_PLAN,
+                               action_dim=config["policy"]["act_dim"],
+                               combine=config["policy"]["combine_scans"])
+
+    sac = SAC(actor_critic=ac,
+              gamma=config["SAC"]["gamma"], 
+              polyak=config["SAC"]["polyak"], 
+              lr=config["SAC"]["lr"], 
+              alpha=config["SAC"]["alpha"])
+
+    replay = ReplayBuffer(
+        obs_dim=config["policy"]["obs_dim"], 
+        act_dim=config["policy"]["act_dim"], 
+        size=config["train"]["replay_size"]
+    )
+    sac.save(MODEL_STORAGE)
+
+    # Launch simulation
+    time.sleep( torch.rand(1)*10 )
+    launch_simulation(uuid=TASK_UUID, args='gui:=true')
+
+    # Main loop
+    ## Generate commands
+    choose_opponent()
+    with open(EXPLORE_CMD, 'w') as f:
+        pass
+    ## Run infinite looping episode
+    train_ep_proc = run_episode(output_path=TRAIN_EPISODE, model_path=MODEL, explore=EXPLORE_CMD)
+    while not os.path.exists(TRAIN_EPISODE):
+        time.sleep(0.01)
+
+    explore = True
+    total_steps = 0
+    for epoch in range(config["epoch"]):
+        start_time = time.time()
+        while total_steps // config["steps_per_epoch"] == epoch:
+            # Begin episode
+            if explore is True and total_steps >= config["train"]["explore_steps"]:
+                explore = False
+                os.remove(EXPLORE_CMD)
+
+            # Wait for the new episode
+            while not os.path.exists(TRAIN_EPISODE):
+                time.sleep(0.01)
+            time.sleep(0.1)
+
+            # Use previous episode to train policy
+            choose_opponent()   # choose opponent robot behavior for the next episode
+            s, a, ns, r, d = load_data(TRAIN_EPISODE)
+            ep_len = replay.store(s, a, ns, r, d)
+            total_steps += ep_len
+            tensorboard.add_scalars("reward", {"train": r.sum()}, total_steps)
+
+            if total_steps > config["train"]["update_after"]:
+                for k in range(total_steps-ep_len, total_steps):
+                    batch = replay.sample_batch(config["train"]["batch_size"])
+                    q_info, pi_info = sac.update(batch=batch)
+                    tensorboard.add_scalar("Loss/Pi", pi_info.detach().item(), k)
+                    tensorboard.add_scalar("Loss/Q",  q_info.detach().item(), k)
+                sac.save(MODEL_STORAGE)
+
+        """ No test recording with (Loop-episode + No MPI)
+        # For each Epoch, run test episode to show training progress
+        test_episode = run_episode(output_path=TEST_EPISODE, model_path=MODEL, test=True)
+        test_episode.wait()
+        _, _, _, r, _ = load_data(TEST_EPISODE)
+        tensorboard.add_scalars("reward", {"test": r.sum()}, total_steps)
+        """
+        # Save policy for every $SAVE_FREQUENCY epochs
+        if (epoch+1) % config["save_freqency"] == 0:
+            sac.save(os.path.join(MODEL_STORAGE, str(epoch+1)))
+
+        print(f"Epoch {epoch+1} took {time.time() - start_time:.2f} seconds.", flush=True)
+
+    tensorboard.close()
+    train_ep_proc.kill()
+    # test_episode.kill()
